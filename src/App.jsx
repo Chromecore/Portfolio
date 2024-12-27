cdba04f3e51f172f9f2c6a3d9ba3e9738f7b1040
Tayin Wallace <runnotsoslow@outlook.com>
2024-12-27 14:41:36 -0700

    Commented extra base setup

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App()
 
 
 
-      <div className="card">
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -33,7 +33,7 @@ function App()
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
