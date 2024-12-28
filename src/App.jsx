3216e4e5649c7ed88baec3b5168141b1f04dbd5d
Tayin Wallace <runnotsoslow@outlook.com>
2024-12-28 11:48:00 -0700

    Fixed contact error

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,9 @@ import { useState } from 'react'
 import './App.css'
 import Header from './Header.jsx'
 import About from './about/About.jsx'
-import Contact from './contact.jsx'
+import Contact from './Contact.jsx'
 
-function App()
-{
+function App() {
   const [count, setCount] = useState(0)
 
   return (
