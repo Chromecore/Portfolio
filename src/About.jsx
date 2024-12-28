f1c70aeae3e6c602ea6801ca9d9ce5f0b2e0a3bd
Tayin Wallace <runnotsoslow@outlook.com>
2024-12-27 18:42:06 -0700

    Fixed some sizing

--- src/About.jsx
+++ src/About.jsx
@@ -1,6 +1,7 @@
 import './About.css'
 import ContactIcon from './assets/icons/contact'
 import ArrowIcon from './assets/icons/arrow'
+import ResumeIcon from './assets/icons/resume'
 import iconsImage from './assets/images/icons.png'
 
 function About()
@@ -18,11 +19,16 @@ function About()
                     <img src={iconsImage} alt="Games and software related icons"></img>
                 </div>
                 <div className="introContent">
-                    <h2>Software/Games</h2>
+                    <h2>Games/Software</h2>
                     <p>
                         <span>&#123;</span>
-                        I program and design games, apps, websites
+                        I program and create games, apps, websites
                         <span>&#125;</span></p>
+                    <a href='https://drive.google.com/file/d/1AmegWO79QVZejVwYmzNuLmxkF4PsZZrg/view?usp=drive_link'
+                        target='blank' className='resume'>
+                        <ResumeIcon />
+                        <span>Resume</span>
+                    </a>
                     <button >
                         <ContactIcon />
                         Contact Me
